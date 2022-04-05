1dcdb19ea125e5ba190ca64ac629fdc885f543f2
Ikechukwu <ikechukwu.njoku@decagon.dev>
2022-04-05 16:05:08 +0100

    Started the singleton design pattern

---
+++ SingletonDemo/SingletonDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SingletonDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Singleton s1 = Singleton.GetInstance;
+            s1.PrintDetails("This is first nessage");
+            Singleton s2 = Singleton.GetInstance;
+            s2.PrintDetails("This is the second message");
+            Console.ReadLine();
+        }
+    }
+}
